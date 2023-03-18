2e2f05f1c26568238480f11511a9d660d8512fc5
Ahmed Hany <shamedhany2001@gmail.com>
2023-03-17 17:26:48 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *main - Entry point
  *Description: generates a random number then guess
  *if its positive, negative or zero
  *Return: Always 0 (Success)
  */
+
+
 int main(void)
 {	int n;
 	srand(time(0));
