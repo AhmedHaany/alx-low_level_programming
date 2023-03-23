b34b5def19d80aeaf35d8e88c05700969f3facfc
Ahmed Hany <shamedhany2001@gmail.com>
2023-03-23 08:43:53 -0700

    task0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * positive_or_negative - check n  whether
+ *        it is positive, negative, or zero.
+ * n parameter - positive or negative or zero
+ * Return: Always 0.
+*/
+
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
