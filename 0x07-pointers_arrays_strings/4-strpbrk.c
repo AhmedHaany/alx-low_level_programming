db4a3dceb6a7509c48a7f1dc1291531205031e3e
Ahmed Hany <shamedhany2001@gmail.com>
2023-04-10 14:35:32 -0700

    task4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates first occurence of a character in accept in s
+ * @s: string to search in
+ * @accept: string to search in
+ *
+ * Return: the start of the first occurance
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return (s + i - 1);
+			}
+		}
+	}
+	return (NULL);
+}
