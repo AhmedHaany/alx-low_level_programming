c107b563556256f7a2861564c4cba201268c6263
Ahmed Hany <shamedhany2001@gmail.com>
2023-04-15 14:53:40 -0700

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_calloc - create array nmemb of size bytes
+ * @nmemb: number of elements
+ * @size: size of byte
+ * Return: a pointer to the allocated memory.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	unsigned int i = 0;
+
+	for (; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
